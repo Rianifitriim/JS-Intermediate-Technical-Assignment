c53eeccd9d29faa5ee7f90fb49248b30a65ad680
Rianifitriim <rianifitri53@gmail.com>
2021-10-10 23:27:53 +0700

    01-array

---
+++ array1.js
@@ -0,0 +1,14 @@
+//soal 1
+const arr = [1, 2, 3];
+const arr2 = [8, 1, 5, 7];
+function reverse() {
+  
+    const new_arr = arr.reverse();
+    const new_arr2 = arr2.reverse();
+    
+    console.log(new_arr);
+    console.log(arr2);
+}
+reverse();
+
+
