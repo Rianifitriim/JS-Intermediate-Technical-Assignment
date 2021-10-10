f82413c49caa5909710040eb466461c7ab017e4b
Rianifitriim <rianifitri53@gmail.com>
2021-10-10 23:28:20 +0700

    02-object

---
+++ obj1.js
@@ -0,0 +1,25 @@
+const person = {
+    name: "person A",
+    age: 100,
+    favDrinks: [
+      "coffee",
+      "jamu temulawak",
+      "tea"
+    ],
+    greeting: function(name) {
+      return `Hello, ${name}`;
+    }
+  }
+  
+  /// EDIT HERE
+  person.name = "Riani"
+  person.favDrinks[1] = "tap water"
+  
+  /// STOP
+  
+  console.log(person.name);
+  console.log(person.age);
+  console.log(person.favDrinks);
+  console.log(person.greeting("John Watson"));
+
+ 
